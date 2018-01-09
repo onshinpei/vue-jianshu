2b7a1390a1f57ede91cd6d4771380a7a8617fb0e
wenxinping66 <244221001@qq.com>
2018-01-09 22:18:18 +0800

    添加推荐列表

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -9,7 +9,21 @@
       <div class="recommend-collection">
         <Badge class="recommend-collection-item" v-for="collection in collections" color="#ea6f5a" :href="collection.href" :key="collection.title">{{collection.title}}</Badge>
       </div>
-      <ul></ul>
+      <div class="flow-list-container">
+        <ul class="flow-list-ul">
+          <li class="flow-list-li" v-for="flowList in flowLists" v-if="flowList.object.data" :key="flowList.object.data.id">
+            <a href="javascript:;">
+              <div class="author">
+                <a href="javascript:;" class="author-avator-a"><img :src="flowList.object.data.user.avatar" alt=""></a>
+                <a href="javascript:;" class="author-anme-a">{{flowList.object.data.user.nickname}}</a>
+              </div>
+              <div class="flow-list-img" :style="'background-image:url('+ flowList.object.data.list_image_url +')'"></div>
+              <div class="title">{{flowList.object.data.title}}</div>
+              <p class="abstract">{{flowList.object.data.public_abbr}}</p>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -31,7 +45,8 @@
         },
         collections: [],
         // 换一批
-        exceptCollectionIds: []
+        exceptCollectionIds: [],
+        flowLists: []
       }
     },
     mounted () {
@@ -74,7 +89,8 @@
             Accept: 'application/json'
           }
         }).then((res) => {
-          console.log(res)
+          this.flowLists = res.data
+          console.log(this.flowLists)
         })
       }
     }
@@ -98,4 +114,54 @@
     margin: 0 0.1rem 0.1rem
   }
 }
+  .flow-list-li {
+    padding: 0.2rem 0.3rem;
+    .flow-list-img {
+      margin: 15px 0;
+      height: 120px;
+      background-color: #eaeaea;
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-position: 50%;
+    }
+    .title {
+      margin: 10px 0 0;
+      font-size: 0.36rem;
+      font-weight: 400;
+      line-height: 1.4;
+      color: #333;
+    }
+    .abstract {
+      margin: 7px 0 0;
+      line-height: 1.5;
+      display: -webkit-box;
+      -webkit-line-clamp: 2;
+      -webkit-box-orient: vertical;
+      font-size: 13px;
+      color: #999;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+    .author {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      .author-avator-a {
+        display: block;
+      }
+      .author-anme-a {
+        display: block;
+        padding-left: 0.1rem;
+        font-size: 0.26rem;
+        font-weight: bold;
+      }
+
+      img {
+        display: inline-block;
+        width: 34px;
+        border-radius: 50%;
+
+      }
+    }
+  }
 </style>
